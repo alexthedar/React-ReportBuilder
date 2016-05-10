1a9604fbeef50f16e9d4fe1ae1f9e44d3e3e3f25
Alexandar Castaneda <alex@alexthedar.com>
2016-05-10 13:46:12 -0700

    not working.  adding webpack and babel

---
+++ README.md
@@ -0,0 +1,3 @@
+##React Report
+
+This is a simple 2 page app which allows user to select a report and then display data in a table.
---
+++ app/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TablePage from './TablePage';
+
+export default class App extends React.Component {
+  render() {
+    return <TablePage />;
+  }
+}
